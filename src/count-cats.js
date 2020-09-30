4b53564ab9291d96976188a486b56d85ff588036
TatsianaKhadasevich <tatsyanakhodasevich@gmail.com>
2020-09-30 14:06:16 +0300

    feat: added partial solution for 3 tasks

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,31 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+
+  let count = 0;
+  for (let i=0; i < matrix.length; i++ ) {
+  for (let n=0; n < matrix[i].length; n++ ){
+  if( matrix[i][n] === '' || matrix[i][n] === null ||
+  typeof matrix[i][n] !== 'string') {
+  count +=0;
+  } else {
+  count += matrix[i][n].trim().includes('^^');
+  }
+  }
+  }
+  return count;
+    
 };
+
+
+// let count = 0;
+//   for (let i=0; i < matrix.length; i++ ) {
+//   for (let n=0; n < matrix[i].length; n++ ){
+//   if(matrix[i][n].toString() !== matrix[i][n]) {
+//   count +=0;
+//   } else {
+//   count += matrix[i][n].trim().includes('^^');
+//   }
+//   }
+//   }
+//   return count;
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,40 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if(members === '') return;
+  let arr='';
+  members = members.map(item => item.trim().toUpperCase());
+  members = members.sort();
+  for (let i = 0; i < members.length; i++){
+    if(typeof members[i] !== 'string') {
+      arr += '';
+    }
+    else {
+      arr += members[i].slice(0,1);
+    }
+  }
+    return arr;
+  
 };
+
+
+
+
+
+//   return
+//     members.forEach((element) => {
+//       element.slice(0,1)}).sort().join('')
+ 
+// without type check
+
+//       let arr='';  
+//       for (let i = 0; i < members.length; i++){
+//         if(typeof members.sort()[i] !== 'String') {
+//           arr += '';
+//         }
+//         else {
+//           arr += members.sort()[i].slice(0,1);
+//         }
+//       }
+//         return arr;
+      
