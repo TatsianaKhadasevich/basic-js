df4cde87fb08b3a706069f1fe97b29561276ca8a
TatsianaKhadasevich <tatsyanakhodasevich@gmail.com>
2020-11-01 23:35:38 +0300

    feat:added 2 tasks

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,36 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options=null) {
+  let res = '';
+  if(options == null) return str.toString();
+  let optionsObj = 
+  {
+    repeatTimes: (options.repeatTimes == null || typeof options.repeatTimes == 'undefined' )? 1:options.repeatTimes, 
+    separator: options.separator || '+',
+    addition: (typeof options.addition == 'undefined')? '': options.addition, 
+    additionRepeatTimes: (options.additionRepeatTimes == null || typeof options.additionRepeatTimes == 'undefined')? 1:options.additionRepeatTimes, 
+    additionSeparator: options.additionSeparator || '|',  
+  };
+
+
+
+  //optionsObj = this.options;
+
+    for (let i = 1; i <= optionsObj.repeatTimes; i++) {
+    if (optionsObj.additionRepeatTimes != null || typeof optionsObj.additionRepeatTimes != 'undefined') {
+          for (let n = 1; n <= optionsObj.additionRepeatTimes; n++){
+        if(n == optionsObj.additionRepeatTimes) {
+          res += str.toString() + optionsObj.addition.toString();          
+        } else{
+          res += str.toString() + optionsObj.addition.toString() + optionsObj.additionSeparator;
+        }
+      }
+      res += optionsObj.separator;
+    } else {
+    	res += str.toString() + optionsObj.separator
+    } 
+    }
+
+    return res.slice(0,-optionsObj.separator.length) || res;
 };
   
